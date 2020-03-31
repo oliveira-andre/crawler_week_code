47741a4445187363e6b7db479da63ee0ad6707e5
Andre Oliveira <desmonplays@gmail.com>
2020-03-31 18:48:40 -0400

    (new feature) adding name main link and link search

--- app/service/search_remote_jobs_service.rb
+++ app/service/search_remote_jobs_service.rb
@@ -5,9 +5,12 @@ class SearchRemoteJobsService
 
   def initialize(params)
     @link = params.dig('link')
+    @main_link = params.dig('main_link')
+    @link_search = params.dig('link_search')
     @card_group_search = params.dig('card_group')
     @title_search = params.dig('title_search')
     @amount_search = params.dig('amount_search')
+    @name = params.dig('name')
   end
 
   def execute
