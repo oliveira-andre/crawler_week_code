adc8b4614848059759738166dbaaeb3404923ebe
Andre Oliveira <desmonplays@gmail.com>
2020-03-31 18:48:24 -0400

    (new feature) concating main link with job_link

--- app/service/concerns/serializer.rb
+++ app/service/concerns/serializer.rb
@@ -4,13 +4,20 @@ module Serializer
   def serialize(card)
     {
       title: card.search(@title_search).text.strip,
-      amount: card.search(@amount_search).text.delete("\n").squeeze(' ').strip,
-      link: job_link(card)
+      amount: load_amount(card),
+      link: job_link(card),
+      name: @name
     }.compact
   end
 
+  def load_amount(card)
+   @amount = card.search(@amount_search).text.delete("\n").squeeze(' ').strip
+   @amount&.match?(MONEY_PREFIX) ? @amount : 'To Combine or not informed'
+  end
+
   def job_link(card)
-    @job_link = card.search(@link_search).text
-    @job_link.match?(SITE_PREFIXES) ? @job_link : @link + @job_link
+    job_link = card.search(@link_search).attr('href')
+    job_link = job_link.text if job_link&.text
+    @main_link + job_link
   end
 end
