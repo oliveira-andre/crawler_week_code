09f9070448a1949b86edd69eb62689e80ba1a220
Andre Oliveira <desmonplays@gmail.com>
2020-03-31 00:55:27 -0400

    (new feature) executing service based on data from sites util

--- app.rb
+++ app.rb
@@ -1,11 +1,18 @@
+# frozen_string_literal: true
+
 require 'json'
-require 'sinatra'
-Dir["./app/*/*.rb"].each { |file| require file }
+Dir['./app/*/*/*.rb'].sort.each { |file| require file }
+Dir['./app/*/*.rb'].sort.each { |file| require file }
 
 class App < Sinatra::Base
   get '/' do
     content_type :json, charset: 'utf-8'
-    {}.to_json
+    { data: load_jobs }.to_json
+  end
+
+  def load_jobs
+    SitesUtil.params.each do |params|
+      return SearchRemoteJobsService.new(params).execute
+    end
   end
 end
-
